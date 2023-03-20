c6aedb1388a384220df982986812c685f18abb5d
Yisus <jesus.macias@devo.com>
2023-03-20 08:26:38 +0100

    feat - asset component is created

---
+++ src/components/Assets.tsx
@@ -0,0 +1,12 @@
+// components
+import { Box, Heading } from "@chakra-ui/react";
+
+const Assets = () => (
+  <Box>
+    <Heading as="h3" size="md">
+      Assets tab container, pending development 👨🏽‍💻
+    </Heading>
+  </Box>
+);
+
+export { Assets };
