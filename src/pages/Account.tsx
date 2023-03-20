a6000525ae4bcccc43723958ae00a9589bca36f7
Yisus <jesus.macias@devo.com>
2023-03-20 08:29:14 +0100

    feat - Account page is created

---
+++ src/pages/Account.tsx
@@ -0,0 +1,62 @@
+// components
+import { Box, Center, Heading, Spinner, Text } from "@chakra-ui/react";
+import {
+  TabsContainer,
+  TabsLink,
+  Assets,
+  TransactionList,
+} from "../components/";
+
+// context
+import { Tabs } from "../context/Tabs";
+
+const Account = ({
+  transactions,
+  accountDetails,
+  searchTransanction,
+  isLoading,
+}) => {
+  const account = accountDetails.accountId;
+
+  const handleSearchTransanction = (accountId) => () =>
+    searchTransanction(accountId);
+
+  return (
+    <Box paddingTop="18">
+      <Heading as="h1" size="xl" lineHeight="4.8rem">
+        {account}
+      </Heading>
+
+      {isLoading && (
+        <Center paddingY={40}>
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        </Center>
+      )}
+
+      {!isLoading && (
+        <Tabs>
+          <TabsLink>
+            <Text>Transactions</Text>
+            <Text>Assets</Text>
+          </TabsLink>
+
+          <TabsContainer>
+            <TransactionList
+              transactions={transactions}
+              handleSearchTransanction={handleSearchTransanction}
+            />
+            <Assets />
+          </TabsContainer>
+        </Tabs>
+      )}
+    </Box>
+  );
+};
+
+export { Account };
